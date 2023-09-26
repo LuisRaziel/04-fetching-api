a0edc18ff1c59572bbbd636f4d5f353efbc13b30
Luis Raziel <razieldiazh@gmail.com>
2023-09-25 22:14:41 -0600

    separate the logic with custom hooks and add the most importan test

---
+++ src/services/facts.js
@@ -0,0 +1,7 @@
+const CAT_ENDPOINT_FACT = 'https://catfact.ninja/fact'
+export const getRandomFact = async () => {
+  const response = await fetch(CAT_ENDPOINT_FACT)
+  const data = await response.json()
+  const { fact } = data
+  return fact
+}
